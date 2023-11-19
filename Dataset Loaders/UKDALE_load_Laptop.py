f7b5a78597e6d1cdc72fe7fcdae9b8781f603848
Jack <jmckenna5225@gmail.com>
2023-11-19 20:06:10 +0000

    duration testings

--- Dataset Loaders/UKDALE_load_Laptop.py
+++ Dataset Loaders/UKDALE_load_Laptop.py
@@ -1,19 +1,23 @@
 from nilm_analyzer.loaders import UKDALE_Loader
 import pandas as pd
 
-from utilities import convert_timestamps2minutes
 ukdale = UKDALE_Loader(data_path=r'C:\Users\44749\NILM\nilm_analyzer_EM401\UKDALE')
+
+print(ukdale.get_appliance_names(house =5))
+
+
+
 
 ##Takes specific appliance data for whatever combination of houses
 #a = ukdale.get_appliance_data(appliance="dishwasher", houses=[1])
 
 ##Takes whole dataset for individual house, including aggregate and all appliances
-a = ukdale.get_house_data(house = 4)
+#a = ukdale.get_house_data(house = 4)
 
-b = convert_timestamps2minutes(pd.Series(a))
+#b = convert_timestamps2minutes(pd.Series(a))
 #c = a.data
 #d = print(a.data)
-print(b)
+
 
 #df = pd.DataFrame(a,None,None)
 
